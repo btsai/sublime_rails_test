5c7d4d1500b6a9229f694a8547449c6e7a29d5cc
btsai <btsai@cerego.co.jp>
2013-11-19 07:40:25 +0900

    adding rails test initiator

---
+++ RailsTest.py
@@ -0,0 +1,151 @@
+import sublime, sublime_plugin, os, re, fnmatch, subprocess
+
+class FindRailsFiles():
+  def set_project_folder(self):
+    self.project_folder = None if len(self.window.folders()) == 0 else self.window.folders()[0]
+    return self.project_folder
+
+  def check_if_test_file(self, filename):
+    testfile_check = re.match(r"^(.+?)_test\.rb", filename)
+    code_file = testfile_check.group(1) + '.rb' if testfile_check else None
+    return testfile_check, code_file
+
+  def open_code_file(self, code_file):
+    filepath = self.get_code_filepath(code_file)
+    if filepath:
+      self.window.open_file(filepath)
+
+  def open_test_file(self, test_file):
+    filepath = self.get_test_filepath(test_file)
+    if filepath:
+      self.window.open_file(filepath)
+
+  def get_code_filepath(self, code_file):
+    filepath = self.model_path(code_file) or self.controller_path(code_file)
+    return filepath
+
+  def get_test_filepath(self, test_file):
+    filepath = self.unit_test_path(test_file) or self.functional_test_path(test_file)
+    return filepath
+
+  def model_path(self, filename):
+    return self.recursive_find('app/models', filename)
+
+  def controller_path(self, filename):
+    return self.recursive_find('app/controllers', filename)
+
+  def unit_test_path(self, filename):
+    return self.recursive_find('test/unit', filename)
+
+  def functional_test_path(self, filename):
+    return self.recursive_find('test/functional', filename)
+
+  def recursive_find(self, relative_folder, filename):
+    base_folder = os.path.join(self.project_folder, relative_folder)
+    finds = self.recursive_glob(base_folder, filename)
+    if len(finds) == 0:
+      return None
+    else:
+      return finds[0]
+
+  def recursive_glob(self, base_folder, pattern):
+    results = []
+    for base, dirs, files in os.walk(base_folder):
+      goodfiles = fnmatch.filter(files, pattern)
+      results.extend(os.path.join(base, f) for f in goodfiles)
+    return results
+
+
+class RailsTestCommand(FindRailsFiles, sublime_plugin.WindowCommand):
+  def run(self):
+    if not self.set_project_folder():
+      return
+
+    filepath = self.window.active_view().file_name()
+    filename = os.path.basename(filepath)
+    if os.path.splitext(filename)[1] != '.rb':
+      # ignore non-rb files
+      return
+    is_test_file, code_file = self.check_if_test_file(filename)
+    if is_test_file:
+      test_filepath = filepath
+    else:
+      test_file = filename.replace('.rb', '') + '_test.rb'
+      test_filepath = self.get_test_filepath(test_file)
+      if not test_filepath:
+        # couldn't find a matching test file
+        return
+
+    settings = sublime.load_settings('RailsTest.sublime-settings')
+    osascript = settings.get("osascript") or "/usr/bin/osascript"
+    applescript_path = "{packages_dir}/RailsTest/rails_test.applescript".format(
+      packages_dir = sublime.packages_path(),
+      terminal_name = settings.get("terminal")
+    )
+    project_settings = self.ensure_project_settings_has_rails_test_settings()
+
+    apple_commands = [
+      osascript,
+      applescript_path,
+      project_settings['rvm_initialization'],
+      self.project_folder,
+      re.sub(self.project_folder + '/', '', test_filepath)
+    ]
+
+    view = self.window.active_view()
+    selected_text = view.substr(view.sel()[0])
+    if selected_text != "":
+      apple_commands.append(selected_text)
+
+    subprocess.Popen(apple_commands)
+
+
+  def ensure_project_settings_has_rails_test_settings(self):
+    settings = self.window.project_data()
+    if self.project_settings_key in settings:
+      # ensure that all keys have a default, in case something got deleted, by merging with the default settings.
+      # any existing settings will win.
+      full_settings = dict( list(self.default_settings.items()) + list(settings[self.project_settings_key].items()) )
+
+      # save back to project settings if there are changes
+      if settings[self.project_settings_key] != full_settings:
+        sublime.status_message("Updated project settings.")
+        self.window.set_project_data(settings)
+    else:
+      # not set up yet, so add the autocomplete settings node to the project settings
+      settings[self.project_settings_key] = self.default_settings
+      self.window.set_project_data(settings)
+
+    return settings[self.project_settings_key]
+
+
+  @property
+  def project_settings_key(self):
+    return 'rails_test_settings'
+
+  @property
+  def base_path(self):
+    return os.path.join(sublime.packages_path(), 'RailsTest')
+
+  @property
+  def default_settings(self):
+    return {
+      'rvm_initialization': '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"; rvm 1.9.3; ruby -v',
+    }
+
+
+class ToggleRailsTestFileCommand(FindRailsFiles, sublime_plugin.WindowCommand):
+  def run(self):
+    if not self.set_project_folder():
+      return
+
+    filepath = self.window.active_view().file_name()
+    filename = os.path.basename(filepath)
+
+    is_test_file, code_file = self.check_if_test_file(filename)
+    if is_test_file: # is test
+      self.open_code_file(code_file)
+    else:     # is code
+      test_file = filename.replace('.rb', '') + '_test.rb'
+      self.open_test_file(test_file)
+
---
+++ rails_test.applescript
@@ -0,0 +1,116 @@
+global ruby_initialization, project_folder, test_file, test_name
+
+on run argv
+	if (count of argv) is 0 then
+		--DEBUGGING USE IN APPLESCRIPT EDITOR
+		set ruby_initialization to "19"
+		set project_folder to "/Users/btsai/git/workcloud/workcloud"
+		set test_file to "test/unit/relationship_test.rb"
+		set test_name to "test_initial_setup"
+	else
+		--SHOULD HAVE 3 + 1 OPTIONAL ARGUMENTS PASSED OVER BY SUBLIME SCRIPT
+		set ruby_initialization to item 1 of argv
+		set project_folder to item 2 of argv
+		set test_file to item 3 of argv
+		set test_name to null
+		if (count of argv) is 4 then
+			set test_name to item 4 of argv
+		end if
+	end if
+
+	tell application "iTerm"
+		set session_name to the last word in test_file
+		if test_name is not null then
+			set session_name to session_name & "-" & test_name
+		end if
+
+		set test_session to my existing_session_named(session_name)
+		if test_session is null then
+			set test_session to my new_session_named(session_name, ruby_initialization, project_folder)
+		end if
+
+		select test_session
+		tell test_session
+			activate
+
+			if my is_in_repeat_mode(test_session) then
+				set test_command to "y"
+			else
+				set test_command to "ruby " & test_file & ""
+				if test_name is not null then
+					set test_command to test_command & " -n " & test_name
+				end if
+			end if
+
+			write text test_command
+		end tell
+
+	end tell
+
+end run
+
+on is_in_repeat_mode(_session)
+	set last_line to my last_line_in_session(text of _session)
+	if last_line is "Do you want to run this suite again [y/n]?" then
+		return true
+	else
+		return false
+	end if
+end is_in_repeat_mode
+
+on last_line_in_session(buffer_text)
+	set last_line to null
+	set session_lines to reverse of every paragraph of buffer_text
+	repeat
+		set first_word to first item of session_lines
+		if first_word is "" then
+			set session_lines to rest of session_lines
+		else
+			set last_line to first_word
+			exit repeat
+		end if
+	end repeat
+	return last_line
+end last_line_in_session
+
+on existing_session_named(name_to_find)
+	tell application "iTerm"
+		set test_session to null
+		repeat with _session in every session of the current terminal
+			if my session_is_named(_session, name_to_find) then
+				set test_session to _session
+				exit repeat
+			end if
+		end repeat
+		return test_session
+	end tell
+end existing_session_named
+
+on new_session_named(new_name)
+	tell application "iTerm"
+		tell the current terminal
+			launch session "Default Session"
+			set name of current session to new_name
+			tell current session
+				activate
+				write text ruby_initialization
+				write text "cd " & project_folder & ""
+			end tell
+			return current session
+		end tell
+	end tell
+end new_session_named
+
+on session_is_named(_session, name_to_find)
+	set _session_name to name of _session
+	set match_length to length of name_to_find
+	if length of _session_name ³ match_length then
+		set base_name to text 1 thru match_length of _session_name
+		if base_name is name_to_find then
+			return true
+		end if
+	end if
+	return false
+end session_is_named
+
+
